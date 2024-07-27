d99aa9c7228d0f7f740d7368b69b56ce24728c8c
Saud Sattar <83525131+saud-sattar@users.noreply.github.com>
2024-07-27 18:44:20 +1000

    headers completed

---
+++ components/Nav.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { CloudCog } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: 'home',
+        path: "/",
+    },
+    {
+        name: 'projects',
+        path: "/projects",
+    },
+    {
+        name: 'resume',
+        path: "/resume",
+    },
+    {
+        name: 'contact',
+        path: "/contact",
+    },
+]
+
+const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+                return (
+                    <Link
+                        href={link.path}
+                        key={index}
+                        className={`
+                            ${link.path === pathname && "text-accent border-b-2 border-accent"}
+                        capitalize font-medium hover:text-accent transition-all`}
+                    >
+                        {link.name}
+                    </Link>
+                );
+            })}
+        </nav>
+    );
+};
+
+export default Nav;
